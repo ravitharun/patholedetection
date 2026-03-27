b5cf1478f4d34c8757ad136bfd537c88769cd789
Ravi Tharun <tr565003@gmail.com>
2026-03-27 18:48:35 +0530

    Web-->PWA

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,107 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { VitePWA } from "vite-plugin-pwa";
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(), tailwindcss(),
+//     VitePWA({
+//       registerType: "autoUpdate",
+//       manifest: {
+//         name: "PathoDetect",
+//         short_name: "PathoDetect",
+//         start_url: "/",
+//         display: "standalone",
+//         theme_color: "#1c1c1cff",
+//         background_color: "#ffffff",
+//         icons: [
+//           {
+//             src: "/icon-192.png",
+//             sizes: "192x192",
+//             type: "image/png",
+//           },
+//           {
+//             src: "/icon-512.png",
+//             sizes: "512x512",
+//             type: "image/png",
+//           },
+//         ],
+//       }
+
+//     }),
+//   ],
+
+//   server: {
+//     host: true,
+//     port: 5173,
+//     allowedHosts: [
+//       "unintellectual-pily-christia.ngrok-free.dev",
+//     ],
+//     proxy: {
+//       "/api": {
+//         // target: "http://localhost:3001",
+//         target: "https://data.traffic.hereapi.com",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '')
+//         // changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    react(), tailwindcss(),
+    react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true, // enables PWA in dev
+      },
+
       manifest: {
+        id: "/",
         name: "PathoDetect",
         short_name: "PathoDetect",
+        description: "PathoDetect Medical App",
         start_url: "/",
+        scope: "/",
         display: "standalone",
-        theme_color: "#1c1c1cff",
+        theme_color: "#1c1c1c",
         background_color: "#ffffff",
+
         icons: [
           {
-            src: "/icon-192.png",
+            src: "/icons/icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512.png",
+            src: "/icons/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
         ],
-      }
-
+      },
     }),
   ],
 
   server: {
     host: true,
     port: 5173,
-    allowedHosts: [
-      "unintellectual-pily-christia.ngrok-free.dev",
-    ],
+    allowedHosts: ["unintellectual-pily-christia.ngrok-free.dev"],
     proxy: {
       "/api": {
-        // target: "http://localhost:3001",
         target: "https://data.traffic.hereapi.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-        // changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
